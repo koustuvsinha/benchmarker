a350ac5b651e0a7d09b24bf7ead2139002a04f17
Koustuv Sinha <koustuvsinha@hotmail.com>
2015-05-28 21:36:55 +0530

    Update SQLite Helper class

--- app/src/main/java/com/koustuvsinha/benchmarker/databases/DbSQLiteHelper.java
+++ app/src/main/java/com/koustuvsinha/benchmarker/databases/DbSQLiteHelper.java
@@ -1,15 +1,23 @@
 package com.koustuvsinha.benchmarker.databases;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import com.koustuvsinha.benchmarker.models.DbTestRecordModel;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by koustuv on 27/5/15.
  */
-public class DbSQLiteHelper extends SQLiteOpenHelper {
+public class DbSQLiteHelper extends SQLiteOpenHelper implements DbTestInterface {
 
     public static final String TABLE_TEST = "testing";
     public static final String COLUMN_ID = "_id";
@@ -47,5 +55,59 @@ public class DbSQLiteHelper extends SQLiteOpenHelper {
                         + newVersion + ", which will destroy all old data");
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_TEST);
         onCreate(db);
+    }
+
+    public void insertData(List<DbTestRecordModel> modelList) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values;
+        db.beginTransaction();
+
+        Iterator<DbTestRecordModel> it = modelList.iterator();
+        while(it.hasNext()) {
+            DbTestRecordModel model = it.next();
+            values = new ContentValues();
+            values.put(COLUMN_NAME,model.getName());
+            values.put(COLUMN_ADDRESS,model.getAddress());
+            values.put(COLUMN_AGE,model.getAge());
+            db.insert(TABLE_TEST,null,values);
+        }
+        db.setTransactionSuccessful();
+        db.endTransaction();
+
+        db.close();
+    }
+
+    public List<DbTestRecordModel> getData() {
+
+        ArrayList<DbTestRecordModel> dataList;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.query(TABLE_TEST,new String[]
+                { COLUMN_ID, COLUMN_NAME, COLUMN_AGE, COLUMN_ADDRESS},null,null,null,null,null);
+        if(cursor!=null)
+            cursor.moveToFirst();
+
+        dataList  = new ArrayList<DbTestRecordModel>();
+
+        while(cursor.moveToNext()) {
+            DbTestRecordModel rec = new DbTestRecordModel();
+            rec.setId(cursor.getLong(0));
+            rec.setName(cursor.getString(1));
+            rec.setAddress(cursor.getString(2));
+            rec.setAge(cursor.getInt(3));
+            dataList.add(rec);
+        }
+
+        cursor.close();
+
+        return dataList;
+    }
+
+    public void updateData(List<DbTestRecordModel> modelList) {
+
+    }
+
+    public void deleteAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_TEST,"1 = 1",null);
     }
 }
--- app/src/main/java/com/koustuvsinha/benchmarker/databases/DbTestInterface.java
+++ app/src/main/java/com/koustuvsinha/benchmarker/databases/DbTestInterface.java
@@ -1,12 +1,16 @@
 package com.koustuvsinha.benchmarker.databases;
 
+import com.koustuvsinha.benchmarker.models.DbTestRecordModel;
+
+import java.util.List;
+
 /**
  * Created by koustuv on 27/5/15.
  */
 public interface DbTestInterface {
 
-    public void insert();
-    public void read();
-    public void update();
-    public void delete();
+    public void insertData(List<DbTestRecordModel> modelList);
+    public List<DbTestRecordModel> getData();
+    public void updateData(List<DbTestRecordModel> modelList);
+    public void deleteAllData();
 }
